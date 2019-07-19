152fb594554e19f3989772d8f9890b110cc42f4e
Steve Archuleta <stevearchuleta@me.com>
2019-07-19 15:11:33 -0700

    intermediate commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// global variables
+      var attack;
+      var defend;
+
+      var attackflower;
+      var defendflower;
+      
+var attackerHP;
+var attackerAP;
+var attackerCAP;
+
+var defenderHP;
+var defenderAP;
+var defenderCAP;
+
+var name;
+
+var yourFlower;
+var yourDefender;
+
+var myFlower = '';
+var mydef = '';
+
+// reset
+function reset() {
+   $('#picRow').show();
+}
